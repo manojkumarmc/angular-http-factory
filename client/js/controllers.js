290c47e61a72eba5438f5a5cb3e5024c1ef52039
Manoj Kumar MC <mamc@cisco.com>
2014-01-09 15:28:51 +0530

    pushing some promise changes

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,5 +1,14 @@
 myApp.controller('MyCtrl', ['$scope','$http', 'itemService', function($scope, $http, itemService) {
     $scope.getIt = function() {
-        $scope.items = itemService.getAllItems()
+        $scope.items = []
+
+        itemService.getAllItems().then(function(data){
+          $scope.items = data
+          $scope.error = ""
+        },
+        function(errorMessage) {
+            $scope.error = errorMessage
+        })
+
     }
 }])
